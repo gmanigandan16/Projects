a5edb4d33fa2ce11704bf029f195b51893a5f1d0
Manigandan_Ganesan <Manigandan_Ganesan@epam.com>
2017-06-20 14:54:09 +0530

    Changes 2

--- String Manipulation (Vowels and Consonants)/src/com/epam/stringmanipulation/Impl/StringManipulationParser.java
+++
@@ -1,7 +0,0 @@
-package com.epam.stringmanipulation.Impl;
-
-public class StringManipulationParser {
-	public static String[] parseText(StringBuilder text, String splitType) {
-		return text.toString().split(splitType);
-	}
-}
--- String Manipulation (Vowels and Consonants)/src/com/epam/stringmanipulation/main/ConsoleApp.java
+++ String Manipulation (Vowels and Consonants)/src/com/epam/stringmanipulation/main/ConsoleApp.java
@@ -1,31 +1,73 @@
 package com.epam.stringmanipulation.main;
 
-import com.epam.stringmanipulation.Impl.StringManipulationGetInput;
-import com.epam.stringmanipulation.Impl.StringManipulationComparator;
-import com.epam.stringmanipulation.Impl.StringManipulationMatcherImpl;
-import com.epam.stringmanipulation.Impl.StringManipulationParser;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConsoleApp {
 	private static final String VOWELS_REGEX = "(?i)([a-z&&[aeiou]])";
 	private static final String CONSONANTS_REGEX = "(?i)([a-z&&[^aeiou]])";
 	private static final String SPLIT_TYPE_PERIOD = "\\.";
+	private static Scanner scr = new Scanner(System.in);
 
 	public static void main(String[] args) {
+
 		int vowelsCount = 0;
 		int ConsonantsCount = 0;
 		String[] sentences = null;
 		StringBuilder text;
-		text = new StringBuilder(StringManipulationGetInput.getInputFromConsole());
+		Pattern regexPattern;
+		ConsoleApp consoleApplication = new ConsoleApp();
+
+		text = new StringBuilder(consoleApplication.getInputFromConsole());
 		if (text.length() > 0) {
-			sentences = StringManipulationParser.parseText(text, SPLIT_TYPE_PERIOD);
+			sentences = consoleApplication.parseText(text, SPLIT_TYPE_PERIOD);
 			for (String sentence : sentences) {
 				System.out.println("In sentence : " + sentence);
-				vowelsCount = StringManipulationMatcherImpl.Matcher(sentence, VOWELS_REGEX);
-				ConsonantsCount = StringManipulationMatcherImpl.Matcher(sentence, CONSONANTS_REGEX);
-				StringManipulationComparator.comparator(vowelsCount, ConsonantsCount);
+				regexPattern = consoleApplication.patternCreator(VOWELS_REGEX);
+				vowelsCount = consoleApplication.countWordsMatchesRegex(regexPattern, sentence);
+				regexPattern = consoleApplication.patternCreator(CONSONANTS_REGEX);
+				vowelsCount = consoleApplication.countWordsMatchesRegex(regexPattern, sentence);
+
+				consoleApplication.compareVowelsAndConsonantsCount(vowelsCount, ConsonantsCount);
 			}
 		} else {
 			System.out.println("Entered String is Empty");
 		}
+	}
+
+	public void compareVowelsAndConsonantsCount(int vowelsCount, int ConsonantsCount) {
+		if (vowelsCount > ConsonantsCount) {
+			System.out.println("No of Vowles are greater. Vowels Count : " + vowelsCount + " Consonants Count: "
+					+ ConsonantsCount);
+		} else if (vowelsCount < ConsonantsCount) {
+			System.out.println("No of Consonants are greater. Vowels Count : " + vowelsCount + " Consonants Count: "
+					+ ConsonantsCount);
+		} else {
+			System.out.println("No of Vowles and Consonants are equal. Vowels Count : " + vowelsCount
+					+ " Consonants Count: " + ConsonantsCount);
+		}
+
+	}
+
+	public int countWordsMatchesRegex(Pattern pattern, String text) {
+		int count = 0;
+		Matcher matcher = pattern.matcher(text);
+		while (matcher.find())
+			count++;
+		return count;
+	}
+
+	public String getInputFromConsole() {
+		System.out.println("Enter String: ");
+		return scr.nextLine();
+	}
+
+	public Pattern patternCreator(String regexType) {
+		return Pattern.compile(regexType);
+	}
+
+	public String[] parseText(StringBuilder text, String splitType) {
+		return text.toString().split(splitType);
 	}
 }
